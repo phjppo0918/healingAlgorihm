3acf1f425fee1a8836583b566ae2be1821700fa3
phjppo0918 <phjppo0918@kpu.ac.kr>
2021-02-20 10:54:29 +0000

    1037 ing(2)

--- silver/tier5/qz1037/Main.java
+++ silver/tier5/qz1037/Main.java
@@ -8,6 +8,14 @@ public class Main{
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         
         StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        final int NUMBER_OF_MEASURE = Integer.parseInt(st.nextToken()); //<=50
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int measure[] = new int [NUMBER_OF_MEASURE];
+        for(int i=0;i<NUMBER_OF_MEASURE;i++){
+            measure[i] = Integer.parseInt(br.readLine());  //<=1,000,000
+        }
+        
     }
     
 }
