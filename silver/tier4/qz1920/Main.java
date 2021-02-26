8fb27a60e42fa8fa54c07b47c624a9404e9120b4
phjppo0918 <phjppo0918@kpu.ac.kr>
2021-02-26 14:22:11 +0000

    1920 fin

--- silver/tier4/qz1920/Main.java
+++ silver/tier4/qz1920/Main.java
@@ -1,27 +1,47 @@
-//직사각형에서 탈출
 import java.util.*;
 import java.io.*;
 //import java.math.BigInteger;
 public class Main{
+    public static int recode[];
     public static void main(String [] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         
         final int NUMBER_OF_INPUT =  Integer.parseInt(br.readLine());
-        ArrayList<Long> number = new ArrayList<>();
+        recode = new int [NUMBER_OF_INPUT];
         
         StringTokenizer st = new StringTokenizer(br.readLine());
         for(int i=0;i<NUMBER_OF_INPUT;i++) {
-            number.add(Long.parseLong(st.nextToken()));
+            recode[i] = Integer.parseInt(st.nextToken());
         }
+        Arrays.sort(recode);
         
-        final int REQUEST_OF_NUMBER = Integer.parseInt(br.readLine());
+        final int NUMBER_OF_REQUEST =  Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
-        for(int i=0;i<REQUEST_OF_NUMBER;i++) {
-           long temp = Long.parseLong(st.nextToken());
-            if(number.contains(temp)) {
+        int temp, start,middle,finish;
+        boolean isHave[] = new boolean [NUMBER_OF_REQUEST];
+        for(int i=0;i<NUMBER_OF_REQUEST;i++) {
+            temp = Integer.parseInt(st.nextToken());
+            start = 0;
+            finish = NUMBER_OF_INPUT-1;
+            while(start <= finish) {
+                middle = (start+finish)/2;
+                if(recode[finish]==temp){
+                    isHave[i] = true;   
+                }
+                
+                if(recode[middle]>temp) {
+                    finish = middle-1;
+                }else if(recode[middle]==temp){
+                   isHave[i] = true;
+                    break;
+                }else{
+                    start = middle+1;
+                }
+            }
+            if(isHave[i]){
                 bw.write("1\n");
-            }else {
+            }else{
                 bw.write("0\n");
             }
         }
@@ -32,6 +52,6 @@ public class Main{
         
         bw.flush();
         bw.close();
-    }   
-    
+    }
+
 }
