de3f5328437fb25787e027b119d079f3704d2660
phjppo0918 <phjppo0918@kpu.ac.kr>
2021-03-07 13:40:21 +0000

    2630 fin

--- silver/tier3/qz2630/Main.java
+++ silver/tier3/qz2630/Main.java
@@ -4,8 +4,9 @@ import java.io.*;
 public class Main{
 
   static int LENGTH;
-  byte paper[][];
+  static byte paper[][];
   static int paperCount = 0;
+  static int bluePaperCount = 0;
   public static void main(String [] args) throws Exception {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -18,27 +19,65 @@ public class Main{
     for(int i=0;i<LENGTH;i++) {
       st = new StringTokenizer(br.readLine());
       for(int j=0;j<LENGTH;j++) {
-        paper[i][j] =(byte)Integer.parseInt(st.nextToken());
+        paper[i][j] =(byte)Integer.parseInt(st.nextToken()); //파랑 : 1
       }
     }
+    cutPaper(0,0,LENGTH);
+    bw.write(paperCount-bluePaperCount+"\n");
+    bw.write(bluePaperCount+"\n");
 
-
+      
     bw.flush();
     br.close();
     bw.close();
       
 
   }
-  public static void cutPaper(int x, int y, int size) {
+  public static void cutPaper(int x, int y, int size) { // x 가 행    y 가 열
+    if(size==1) {
+        paperCount++;
+        if(paper[x][y]==1) {
+            bluePaperCount++;
+        }
+        return;
+    }
     //int centerLeft = (start+end)/2;
     byte color = paper[x][y];
-    for(int i=x;i<size;i++) {
-      for(int j=y;j<size;j++) {
-        if(paper[i][j] != color) {
+    boolean isFull = true;
+      
+    for(int i=x;i<x+size;i++) {
+      for(int j=y;j<y+size;j++) {
           
+        if(paper[i][j] != color) {
+            isFull = false;
+            break;
         }
+          
+      }
+      if(!isFull) {
+        break;
       }
     }
+    
+    if(isFull) {
+        paperCount++;
+        if(paper[x][y]==1) {
+            bluePaperCount++;
+        }
+        return;
+    }else {
+        //4방면 호출
+        //1사
+        cutPaper(x,y,size/2);
+        //2사
+        cutPaper(x,y+size/2,size/2);
+        //3사
+        cutPaper(x+size/2,y,size/2);
+        //4사
+        cutPaper(x+size/2,y+size/2,size/2);
+
+    }
+
   }
 
 
