6f86a98f4fb2d39bdacad89b67a59cde25a24fa4
phjppo0918 <phjppo0918@kpu.ac.kr>
2021-03-14 13:25:25 +0000

    5525 fin

--- silver/tier2/qz5525/MainTimeOver.java
+++ silver/tier2/qz5525/MainTimeOver.java
@@ -9,27 +9,34 @@ public class Main{
         //StringTokenizer st;
             //= new StringTokenizer(br.readLine());
         int cycle = Integer.parseInt(br.readLine());
+        int cycleLength = cycle*2+1;
         int totalLength = Integer.parseInt(br.readLine());
-        char str[] = br.readLine().toCharArray();
+        String str = br.readLine();
+        int count = 0;
+        boolean isIO = true;
         
-        int ioCount = 0;
-        int answer = 0;
-        for(int i=1;i<totalLength-1;i++) {
-            if(str[i-1]=='I'&&str[i]=='O'&&str[i+1]=='I') {
-                
-                ioCount++;
-                
-                if(ioCount==cycle) {
-                    answer++;
-                    ioCount--;
+        for(int i=0;i<totalLength-cycleLength+1;i++) {
+            if(str.charAt(i)=='I') {
+                for(int j=i+1;j<i+cycleLength-1;j+=2){
+                    if(str.charAt(j)!='O'||str.charAt(j+1)!='I') {
+                        isIO=false;
+                        break;
+                    }
                 }
-                i++;
-            }else {
-                ioCount = 0;
+                
+                if(str.charAt(i+cycleLength-1)!='I') {
+                    isIO = false;
+                }
+                if(isIO) {
+                    count++;
+                }else {
+                    isIO = true;
+                }
             }
         }
+        bw.write(count+"\n");
         
-        bw.write(answer+"\n");
+        
         
         
         
