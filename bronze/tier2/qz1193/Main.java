ef2039686978ac3a43d13ed0dd1174fb52bc57a4
phjppo0918 <phjppo0918@kpu.ac.kr>
2021-03-27 11:45:01 +0000

    1193 ing

---
+++ bronze/tier2/qz1193/Main.java
@@ -0,0 +1,48 @@
+import java.util.*;
+import java.io.*;
+//import java.math.BigInteger;
+public class Main{
+
+    public static void main(String [] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        //StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        final int X = Integer.parseInt(br.readLine());
+        
+        boolean isUp = false;
+        int count = 0;
+        int layer = 0;
+        int x, y, gap;
+        while( (count + layer + 1) <= X) {
+      
+            layer++;
+            count += layer;
+        }
+        
+        gap = count - X; 
+        if(isUp) {
+            x = layer - gap;
+            y = 1 + gap;
+        }else {
+            y = layer - gap;
+            x = 1 + gap;
+        }
+        
+        
+        sb.append(y + "/"+ x + "\n");
+        bw.write(sb.toString());
+        
+        bw.flush();
+        br.close();
+        bw.close();
+        
+    } 
+    
+}
+
+
+
+
+
