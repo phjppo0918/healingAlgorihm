7582dba4f04cd1be647b89771f334ed3c577a4fe
phjppo0918 <phjppo0918@kpu.ac.kr>
2021-04-24 13:06:46 +0000

    13305 fin:

--- greedy/qz13305/Main.java
+++ greedy/qz13305/Main.java
@@ -1,6 +1,6 @@
 import java.util.*;
 import java.io.*;
-//import java.math.BigInteger;
+import java.math.BigInteger;
 public class Main{
     
     public static void main(String [] args) throws Exception {
@@ -9,42 +9,40 @@ public class Main{
         StringBuilder sb = new StringBuilder();
         StringTokenizer st;// = new StringTokenizer(br.readLine());
         
-				final int NUMBER_OF_CITY = Integer.parseInt(br.readLine());
-
-				st =  new StringTokenizer(br.readLine());
-				int load[] = new int [NUMBER_OF_CITY-1];
-				for(int i=0;i<NUMBER_OF_CITY-1;i++) {
-					load[i] =	Integer.parseInt(st.nextToken());
-				}
-
-				st =  new StringTokenizer(br.readLine());
-				int cost[] = new int [NUMBER_OF_CITY];
-					for(int i=0;i<NUMBER_OF_CITY;i++) {
-					cost[i] =	Integer.parseInt(st.nextToken());
-				}
-				int pivot = 0;
-				//int thisCost = cost[0];
-				while(pivot != NUMBER_OF_CITY-1) {
-					int dest = -1;
-					for(int i=pivot+1;i<NUMBER_OF_CITY;i++) {
-						if(cost[i] < cost[pivot]) {
-							dest = i;
-							break;
-						}
-					}
-
-					if(dest != -1) {
-
-					}else {
-						
-					}
-
-				}
-
-
-			
-
-
+        final int NUMBER_OF_CITY = Integer.parseInt(br.readLine());
+       
+        int road[] = new int [NUMBER_OF_CITY-1];
+        st = new StringTokenizer(br.readLine());
+        for(int i=0;i<NUMBER_OF_CITY-1;i++) {
+            road[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        int cost[] = new int [NUMBER_OF_CITY];
+        st = new StringTokenizer(br.readLine());
+        for(int i=0;i<NUMBER_OF_CITY;i++) {
+            cost[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        int pivot = 0;
+        long answer = 0;
+        while(pivot != NUMBER_OF_CITY-1) {
+            int dest = NUMBER_OF_CITY - 1;
+            for(int i=pivot+1; i<NUMBER_OF_CITY;i++) {
+                if(cost[pivot] > cost[i]) {
+                    dest = i;
+                    break;
+                }
+            }
+            int roadLength = 0;
+            for(int i=pivot;i<dest;i++) {
+                roadLength += road[i];
+            }
+            answer += (long)roadLength * cost[pivot];
+            pivot = dest;
+        }
+        
+        
+        sb.append(answer);
         sb.append("\n"); 
         
         bw.write(sb.toString());
