63066b1280d542bf6b7fd689c39986ac79f2deee
phjppo0918 <phjppo0918@kpu.ac.kr>
2021-04-25 14:12:46 +0000

    1092 fin

--- greedy/qz1092/Main.java
+++ greedy/qz1092/Main.java
@@ -10,73 +10,103 @@ public class Main{
         StringTokenizer st;// = new StringTokenizer(br.readLine());
         final int NUMBER_OF_CRAIN = Integer.parseInt(br.readLine());
         st =  new StringTokenizer(br.readLine());
-        int crainWeight[] = new int [NUMBER_OF_CRAIN];
-        int operLine[] = new int [NUMBER_OF_CRAIN];
-        int operLineWorkLoad[] = new int [NUMBER_OF_CRAIN];
-        int maxCrainWeight = 0;
+        ArrayList <Crain> crain = new ArrayList<Crain>();
+        crain.add(new Crain(0));
         for(int i=0;i<NUMBER_OF_CRAIN;i++) {
-            crainWeight[i] = Integer.parseInt(st.nextToken());
-            operLine[i]++;
-            if(maxCrainWeight < crainWeight[i]) {
-                maxCrainWeight = crainWeight[i];
-            }
+            crain.add(new Crain(Integer.parseInt(st.nextToken()) ) );
         }
-        Arrays.sort(crainWeight); 
+        crain.sort(Comparator.naturalOrder());
+        int maxCrainWeight = crain.get(NUMBER_OF_CRAIN).weight;
+        int railBox[] = new int [NUMBER_OF_CRAIN + 1];
+        for(int i=1;i<=NUMBER_OF_CRAIN;i++) {
+            crain.get(i).rail = i;
+        }
+        int numberOfBox = Integer.parseInt(br.readLine());
+        boolean canOperation = true;
         
-        int NUMBER_OF_BOX = Integer.parseInt(br.readLine());
-        st =  new StringTokenizer(br.readLine());
-        int box;
-        int maxBoxWeight = 0;
-        for(int i=0;i<NUMBER_OF_BOX;i++) {
-            box = Integer.parseInt(st.nextToken());
-            
-            if(maxBoxWeight < box) {
-                maxBoxWeight = box;
+        
+        st = new StringTokenizer(br.readLine());
+        for(int i=0;i<numberOfBox;i++) {
+            int box = Integer.parseInt(st.nextToken());
+            if(maxCrainWeight < box) {
+                canOperation = false;
+                break;
             }
-            
-            for(int j=0;j<NUMBER_OF_CRAIN;J++) {
-                if(box <= crainWeight[j]) {
-                    operLineWorkLoad[j]++;
+            for(int j=1;j<=NUMBER_OF_CRAIN;j++) {
+                if(box <= crain.get(j).weight) {
+                    railBox[j]++;
                     break;
                 }
-            }           
-        }
-        
-        
-        
-        if(maxBoxWeight <= maxCrainWeight) {
-            while(NUMBER_OF_BOX < 1) {
-                int operTime = 0;
-                for(int i=NUMBER_OF_CRAIN-1;i>-1;i--) {
-                    
-                    if(operLineWorkLoad[i] == 0) {
-                        
-                    }
-                    
-                    if(operLineWorkLoad[i] > 0) {
-                        operLineWorkLoad[i] -= operLine[i];
-                        operTime +=  operLine[i];
-                    }
-                    
-                    
-                }
             }
-            
+        }
+        if(canOperation) {
+            int day = 0;
+            while(numberOfBox > 0) {
+                day++;
+                for(int i=NUMBER_OF_CRAIN;i>0;i--) {
+                    
+                    if(crain.get(i).rail == 0) {
+                        continue;
+                    }
+                    
+                    if(railBox[crain.get(i).rail]==0) {
+                        int j;
+                        for(j=crain.get(i).rail;j>0;j--) {
+                            if(railBox[j] != 0) {
+                                crain.get(i).rail = j;
+                                break;
+                            }
+                        }
+                        if(j==0) {
+                            crain.get(i).rail = j;
+                            continue;
+                        }
+                    }
+ 
+                    numberOfBox--;
+                    railBox[crain.get(i).rail]--;
+                } 
+            }
+            sb.append(day);
             
         }else {
-            sb.append(-1)
+            sb.append(-1);
         }
         
         sb.append("\n"); 
         
         bw.write(sb.toString());
         
+        
+        
+        //students.sort(Comparator.reverseOrder());
+
         bw.flush();
         br.close();
         bw.close();
         
     }
 
+    
+}
+class Crain implements Comparable<Crain>{
+    int weight;
+    int rail;
+    
+    @Override
+    public int compareTo(Crain crain) {
+        // TODO Auto-generated method stub
+        if(this.weight == crain.weight) return 0;
+        else if(this.weight >  crain.weight) return 1;
+        else return -1;
+    } 
+
+    
+    public Crain(){}
+    
+    public Crain(int weight) {
+        this.weight = weight;
+    }
     
 }
 
