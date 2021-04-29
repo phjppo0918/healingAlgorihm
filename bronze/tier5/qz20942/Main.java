fcb6fb5f0aab0277e5d4fea9a2453cf9fd0f0b97
phjppo0918 <phjppo0918@kpu.ac.kr>
2021-04-29 14:43:41 +0000

    20942 fin

---
+++ bronze/tier5/qz20942/Main.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.io.*;
+//import java.math.BigInteger;
+public class Main{
+    
+    public static void main(String [] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+       // StringTokenizer st;// = new StringTokenizer(br.readLine());
+        int money = Integer.parseInt(br.readLine());
+        sb.append(money*78/100);
+        sb.append("\n"); 
+        sb.append(money*80/100 + money*20/100*78/100);
+        sb.append("\n"); 
+        
+        bw.write(sb.toString());
+        
+        bw.flush();
+        br.close();
+        bw.close();
+        
+    }
+
+    
+}
+
+
