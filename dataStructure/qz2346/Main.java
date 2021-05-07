7d92128b6408dcf9b95ab555d4b213dcdcfe9d45
phjppo0918 <phjppo0918@kpu.ac.kr>
2021-05-07 14:07:12 +0000

    2346 fin

--- dataStructure/qz2346/Main.java
+++ dataStructure/qz2346/Main.java
@@ -8,33 +8,52 @@ public class Main{
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
         
-        final int NUMBER_OF_BALLOON = Integer.parseInt(br.readLine());
-        Deque <int[]> deque = new LinkedList<int[]>();
+        final short NUMBER_OF_BALLOON = (short)Integer.parseInt(br.readLine());
+        int balloon[] = new int [NUMBER_OF_BALLOON];
         
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i=1;i<=NUMBER_OF_BALLOON;i++) {
-            deque.addLast( new int [] {i, Integer.parseInt(st.nextToken()) } );
+        br.close();
+        for(short i=0;i<NUMBER_OF_BALLOON;i++) {
+           balloon[i] = Integer.parseInt(st.nextToken());
                           
         }
+        short pivot = 0;
+        short boomCount = 0;
         while(true) {
-            int[] boom = deque.pollFirst();
-            sb.append(boom[0]);
+            int boom =  balloon[pivot];
+            sb.append(pivot+1);
             sb.append(" ");
+            balloon[pivot] = 0;
+            boomCount++;
             
-            if(deque.isEmpty()) {
+            if(boomCount==NUMBER_OF_BALLOON) {
                 break;
             }
             
-            if(boom[1] > 0) {
-                for(int i=1;i<boom[1];i++) {
-                    int[] temp = deque.pollFirst();
-                    deque.addLast(temp);
+            if(boom > 0) {
+                for(short i=0;i<boom;) {
+                    pivot++;
+                    if(pivot == NUMBER_OF_BALLOON) {
+                        pivot = 0;
+                    }
+                    
+                    if(balloon[pivot]==0) {            
+                        continue;
+                    }
+                    i++;                  
                 }
-                
             }else {
-                for(int i=0;i<boom[1] * -1;i++) {
-                    int[] temp = deque.pollLast();
-                    deque.addFirst(temp); 
+                for(short i=0;i<(boom * -1);) {
+                    pivot--;
+                    if(pivot == -1) {
+                        pivot = (short)(NUMBER_OF_BALLOON-1);
+                    }
+                    
+                    if(balloon[pivot]==0) {
+                        continue;
+                    }
+                    i++;  
+                    
                 }
             }
         }
@@ -44,7 +63,7 @@ public class Main{
         bw.write(sb.toString());
         
         bw.flush();
-        br.close();
+       // br.close();
         bw.close();
         
     }
