272dc3b38012cfc33f44609547c5eccaaf654647
phjppo0918 <phjppo0918@kpu.ac.kr>
2021-05-07 14:08:47 +0000

    2776 ing

---
+++ dataStructure/qz2776/Main.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.io.*;
+//import java.math.BigInteger;
+public class Main{
+    
+    public static void main(String [] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+       // StringTokenizer st;// = new StringTokenizer(br.readLine());
+        
+        sb.append("\n"); 
+        
+        bw.write(sb.toString());
+        
+        bw.flush();
+        br.close();
+        bw.close();
+        
+    }
+
+    
+}
+
+
