9cf6adb7637802a6c902e93cb9fc63ecf4db1b2d
Hyeonjun Park <phjppo0918@kpu.ac.kr>
2021-06-07 22:14:15 +0900

    1461 fin

--- greedy/qz1461/Main.java
+++ greedy/qz1461/Main.java
@@ -16,63 +16,72 @@ public class Main {
         st = new StringTokenizer(br.readLine());
         PriorityQueue<Integer> plusQ = new PriorityQueue<>(Collections.reverseOrder());
         PriorityQueue<Integer> minusQ = new PriorityQueue<>(Collections.reverseOrder());
-
+        int bestLength = 0;
         for (int i = 0; i < NUMBER_OF_BOOK; i++) {
             int temp = Integer.parseInt(st.nextToken());
             if (temp > 0) {
                 plusQ.add(temp);
             } else {
-                minusQ.add(temp * -1);
+                temp *= -1;
+                minusQ.add(temp);
+            }
+            if (temp > bestLength) {
+                bestLength = temp;
             }
         }
-        int walkCount = (plusQ.peek() > minusQ.peek()) ? plusQ.peek() : minusQ.peek();
+        int walkCount = bestLength;
         walkCount *= -1;
         int bringBook = MAXIMUM_BRING_BOOK;
         int SJLocation = 0;
-        while (true) {
-            int book = plusQ.poll();
-            int walkTemp = SJLocation - book;
-            if (walkTemp < 0) {
-                walkTemp *= -1;
-            }
-            walkCount += walkTemp;
-            SJLocation = book;
-            bringBook--;
+        if (!plusQ.isEmpty()) {
+            while (true) {
+                int book = plusQ.poll();
+                int walkTemp = SJLocation - book;
+                if (walkTemp < 0) {
+                    walkTemp *= -1;
+                }
+                walkCount += walkTemp;
+                SJLocation = book;
+                bringBook--;
 
-            if (plusQ.isEmpty()) {
-                break;
-            }
+                if (plusQ.isEmpty()) {
+                    break;
+                }
 
-            if (bringBook == 0) {
-                walkCount += SJLocation;
-                SJLocation = 0;
-                bringBook = MAXIMUM_BRING_BOOK;
-            }
+                if (bringBook == 0) {
+                    walkCount += SJLocation;
+                    SJLocation = 0;
+                    bringBook = MAXIMUM_BRING_BOOK;
+                }
 
+            }
+            walkCount += SJLocation;
+            SJLocation = 0;
+            bringBook = MAXIMUM_BRING_BOOK;
         }
-        walkCount += SJLocation;
-        SJLocation = 0;
-        bringBook = MAXIMUM_BRING_BOOK;
-        while (true) {
-            int book = minusQ.poll();
-            int walkTemp = SJLocation - book;
-            if (walkTemp < 0) {
-                walkTemp *= -1;
-            }
-            walkCount += walkTemp;
-            SJLocation = book;
-            bringBook--;
+        if (!minusQ.isEmpty()) {
+            while (true) {
+                int book = minusQ.poll();
+                int walkTemp = SJLocation - book;
+                if (walkTemp < 0) {
+                    walkTemp *= -1;
+                }
+                walkCount += walkTemp;
+                SJLocation = book;
+                bringBook--;
 
-            if (minusQ.isEmpty()) {
-                break;
-            }
+                if (minusQ.isEmpty()) {
+                    break;
+                }
 
-            if (bringBook == 0) {
-                walkCount += SJLocation;
-                SJLocation = 0;
-                bringBook = MAXIMUM_BRING_BOOK;
-            }
+                if (bringBook == 0) {
+                    walkCount += SJLocation;
+                    SJLocation = 0;
+                    bringBook = MAXIMUM_BRING_BOOK;
+                }
 
+            }
+            walkCount += SJLocation;
         }
 
 
