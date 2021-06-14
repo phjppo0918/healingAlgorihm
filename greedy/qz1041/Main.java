2345d5cd1396dd5555e8fa4064311a6d615c5255
Hyeonjun Park <phjppo0918@kpu.ac.kr>
2021-06-14 23:24:06 +0900

    1041 fin

--- greedy/qz1041/Main.java
+++ greedy/qz1041/Main.java
@@ -1,7 +1,8 @@
 import java.util.*;
 import java.io.*;
 
-//import java.math.BigInteger;
+import java.math.BigInteger;
+
 public class Main {
 
 
@@ -10,25 +11,71 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
 
-        final int NUMBER_OF_DICE = Integer.parseInt(br.readLine());
+        final BigInteger NUMBER_OF_DICE = new BigInteger(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int dice[] = new int[6];
+        int diceInput[] = new int[6];
         for (int i = 0; i < 6; i++) {
-            dice[i] = Integer.parseInt(st.nextToken());
-        }
-        Arrays.sort(dice);
-        long answer = 0;
-        //꼭지점 3면째
-        answer += dice[2] * 4;
-        //모서리
-        answer += dice[1] * (NUMBER_OF_DICE * 2 + NUMBER_OF_DICE - 2) * 2;
-        //나머지 1면
-        answer += dice[0] *
-                (NUMBER_OF_DICE * NUMBER_OF_DICE
-                        + (NUMBER_OF_DICE * NUMBER_OF_DICE - 2) * 2
-                        + (NUMBER_OF_DICE - 2) * (NUMBER_OF_DICE - 1) * 2);
+            diceInput[i] = Integer.parseInt(st.nextToken());
 
-        sb.append(answer);
+        }
+        int dice[] = new int[3];
+        //   D - C   E - B   A -F
+        dice[0] = Math.min(diceInput[2], diceInput[3]);
+        dice[1] = Math.min(diceInput[1], diceInput[4]);
+        dice[2] = Math.min(diceInput[0], diceInput[5]);
+        Arrays.sort(dice);
+
+        BigInteger answer = BigInteger.ZERO;
+
+        //꼭지점 3면째
+        answer = answer.add(BigInteger.valueOf(dice[2] * 4));
+
+        //모서리
+        // answer += dice[1] * (NUMBER_OF_DICE + NUMBER_OF_DICE - 2) * 4;
+        answer = answer.add(BigInteger.valueOf(dice[1])
+                .multiply(NUMBER_OF_DICE
+                        .add(NUMBER_OF_DICE)
+                        .subtract(BigInteger.valueOf(2)))
+                .multiply(BigInteger.valueOf(4)));
+        //나머지 1면
+        /*
+        answer += dice[0] *
+                (NUMBER_OF_DICE * NUMBER_OF_DICE * 2
+                        + (NUMBER_OF_DICE * (NUMBER_OF_DICE - 2)) * 2
+                        + (NUMBER_OF_DICE - 2) * (NUMBER_OF_DICE - 2));
+
+*/
+        answer = answer.add(BigInteger.valueOf(dice[0])
+                .multiply(
+                        NUMBER_OF_DICE
+                                .multiply(NUMBER_OF_DICE)
+                                .multiply(BigInteger.valueOf(2))
+                                .add(
+                                        NUMBER_OF_DICE
+                                                .multiply(NUMBER_OF_DICE
+                                                        .subtract(BigInteger.valueOf(2)))
+                                                .multiply(BigInteger.valueOf(2))
+                                )
+                                .add(
+                                        NUMBER_OF_DICE.subtract(BigInteger.valueOf(2))
+                                                .multiply(NUMBER_OF_DICE.subtract(BigInteger.valueOf(2)))
+                                )
+                ));
+
+        if (NUMBER_OF_DICE.compareTo(BigInteger.ONE) == 0) {
+            int maxDice = -1;
+            int total = 0;
+            for (int i : diceInput) {
+                total += i;
+                if (i > maxDice) {
+                    maxDice = i;
+                }
+            }
+            sb.append(total - maxDice);
+        } else {
+            sb.append(answer.toString());
+        }
+
         sb.append("\n");
 
 
