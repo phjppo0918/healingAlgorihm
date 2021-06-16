fd19c690ee8a868fbf802522c697b3afead727cc
Hyeonjun Park <phjppo0918@kpu.ac.kr>
2021-06-16 22:25:00 +0900

    1946 fin

--- greedy/qz1946/Main.java
+++ greedy/qz1946/Main.java
@@ -14,16 +14,27 @@ public class Main {
         final int NUMBER_OF_TESTCASE = Integer.parseInt(br.readLine());
         for (int testCase = 0; testCase < NUMBER_OF_TESTCASE; testCase++) {
             int numberOfMember = Integer.parseInt(br.readLine());
-            int member[][] = new int[numberOfMember][2];
+            ArrayList<Member> arrayList = new ArrayList<>();
             for (int i = 0; i < numberOfMember; i++) {
                 st = new StringTokenizer(br.readLine());
-                member[i][0] = Integer.parseInt(st.nextToken());
-                member[i][1] = Integer.parseInt(st.nextToken());
+                int score1 = Integer.parseInt(st.nextToken());
+                int score2 = Integer.parseInt(st.nextToken());
+                arrayList.add(new Member(score1, score2));
             }
 
-            int bestScore1 = member[0][0], bestScore2 = member[0][1];
-            int answer = numberOfMember;
-            fo
+            Collections.sort(arrayList);
+            int passCount = numberOfMember;
+            int bestScore2 = arrayList.get(0).score2;
+            for (int i = 1; i < numberOfMember; i++) {
+                if (arrayList.get(i).score2 > bestScore2) {
+                    passCount--;
+                } else {
+                    bestScore2 = arrayList.get(i).score2;
+                }
+            }
+            sb.append(passCount);
+            sb.append("\n");
+
         }
 
 
@@ -36,5 +47,25 @@ public class Main {
     }
 
 
+}
+
+class Member implements Comparable<Member> {
+    int score1;
+    int score2;
+
+    public Member() {
+    }
+
+    ;
+
+    public Member(int score1, int score2) {
+        this.score1 = score1;
+        this.score2 = score2;
+    }
+
+    @Override
+    public int compareTo(Member member) {
+        return this.score1 - member.score1;
+    }
 }
 
